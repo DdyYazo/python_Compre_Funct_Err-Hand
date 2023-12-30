94370d48e2a194edc486cf31c030beb8b992d9c0
MileColems <sufi.tjof341@gmail.com>
2023-12-30 15:25:05 -0500

    Zen de Python update

---
+++ Zen_python.py
@@ -0,0 +1,27 @@
+import random
+import os 
+os.system("clear")
+
+import this 
+
+# Con esta libreira podemos ver el Zen de Python el cual nos da una serie de consejos para programar en Python
+""" El primero de ellos en español es:	
+1. Hermoso es mejor que feo.
+2. Explícito es mejor que implícito.
+3. Simple es mejor que complejo.
+4. Complejo es mejor que complicado.
+5. Plano es mejor que anidado.
+6. Disperso es mejor que denso.
+7. La legibilidad es importante.
+8. Los casos especiales no son tan especiales como para quebrantar las reglas.
+9. Lo práctico le gana a la pureza.
+10. Los errores nunca deberían pasar silenciosamente.
+11. A menos que se silencien explícitamente.
+12. Frente a la ambigüedad, evitar la tentación de adivinar.
+13. Debería haber una —y preferiblemente solo una— manera obvia de hacerlo.
+14. Aunque esa manera puede no ser obvia al principio a menos que usted sea holandés.
+15. Ahora es mejor que nunca.
+16. Aunque nunca es a menudo mejor que ya mismo.
+17. Si la implementación es difícil de explicar, es una mala idea.
+18. Si la implementación es fácil de explicar, puede que sea una buena idea.
+19. Los espacios de nombres son una gran idea —¡tengamos más de esas!"""
